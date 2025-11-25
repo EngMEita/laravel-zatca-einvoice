a83fd431c46a3d202414119544fea23e7719cc06
Eng. M. Adel Eita <maa1987@hotmail.com>
2025-11-25 10:04:17 +0300

    Update: improve XML namespace + fix seller info + tax rounding

--- src/Support/XmlBuilder.php
+++ src/Support/XmlBuilder.php
@@ -2,72 +2,111 @@
 
 namespace Meita\ZatcaEInvoice\Support;
 
-use SimpleXMLElement;
-
 class XmlBuilder
 {
     public static function build(array $header, array $seller, array $buyer, array $items, array $totals): string
     {
-        $xml = new SimpleXMLElement(
-            '<?xml version="1.0" encoding="UTF-8"?>
-<Invoice xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2"
-    xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2"
-    xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2">
-</Invoice>'
+        $doc = new \DOMDocument('1.0', 'UTF-8');
+        $doc->formatOutput = true;
+
+        // Root Invoice element
+        $invoice = $doc->createElementNS(
+            'urn:oasis:names:specification:ubl:schema:xsd:Invoice-2',
+            'Invoice'
         );
 
+        // Add namespaces
+        $invoice->setAttributeNS(
+            'http://www.w3.org/2000/xmlns/',
+            'xmlns:cac',
+            'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2'
+        );
+
+        $invoice->setAttributeNS(
+            'http://www.w3.org/2000/xmlns/',
+            'xmlns:cbc',
+            'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2'
+        );
+
+        $doc->appendChild($invoice);
+
         // Header
-        $xml->addChild('cbc:ID', $header['number'], 'cbc');
-        $xml->addChild('cbc:IssueDate', $header['date'], 'cbc');
-        $xml->addChild('cbc:IssueTime', $header['time'], 'cbc');
+        self::addCbc($doc, $invoice, 'cbc:ID', $header['number']);
+        self::addCbc($doc, $invoice, 'cbc:IssueDate', $header['date']);
+        self::addCbc($doc, $invoice, 'cbc:IssueTime', $header['time']);
 
         // Seller
-        $supplier = $xml->addChild('cac:AccountingSupplierParty', null, 'cac')
-            ->addChild('cac:Party', null, 'cac');
-
-        $supplier->addChild('cbc:Name', $seller['name'], 'cbc');
-        $supplierTax = $supplier->addChild('cac:PartyTaxScheme', null, 'cac');
-        $supplierTax->addChild('cbc:CompanyID', $seller['vat'], 'cbc');
+        $supplierParty = self::cac($doc, $invoice, 'cac:AccountingSupplierParty');
+        $party = self::cac($doc, $supplierParty, 'cac:Party');
+        self::addCbc($doc, $party, 'cbc:Name', $seller['name']);
+        $taxScheme = self::cac($doc, $party, 'cac:PartyTaxScheme');
+        self::addCbc($doc, $taxScheme, 'cbc:CompanyID', $seller['vat']);
 
         // Buyer
-        $customer = $xml->addChild('cac:AccountingCustomerParty', null, 'cac')
-            ->addChild('cac:Party', null, 'cac');
+        $customerParty = self::cac($doc, $invoice, 'cac:AccountingCustomerParty');
+        $party2 = self::cac($doc, $customerParty, 'cac:Party');
+        self::addCbc($doc, $party2, 'cbc:Name', $buyer['name']);
+        $buyerTax = self::cac($doc, $party2, 'cac:PartyTaxScheme');
+        self::addCbc($doc, $buyerTax, 'cbc:CompanyID', $buyer['vat']);
 
-        if (!empty($buyer['name'])) {
-            $customer->addChild('cbc:Name', $buyer['name'], 'cbc');
-        }
-        if (!empty($buyer['vat'])) {
-            $buyerTax = $customer->addChild('cac:PartyTaxScheme', null, 'cac');
-            $buyerTax->addChild('cbc:CompanyID', $buyer['vat'], 'cbc');
-        }
+        // Invoice lines
+        foreach ($items as $index => $item) {
+            $line = self::cac($doc, $invoice, 'cac:InvoiceLine');
+            self::addCbc($doc, $line, 'cbc:ID', $index + 1);
+            self::addCbc($doc, $line, 'cbc:InvoicedQuantity', $item['qty']);
 
-        // Items
-        foreach ($items as $item) {
-            $line = $xml->addChild('cac:InvoiceLine', null, 'cac');
-            $line->addChild('cbc:ID', $item['id'], 'cbc');
-            $line->addChild('cbc:InvoicedQuantity', $item['qty'], 'cbc');
-            $line->addChild('cbc:LineExtensionAmount', $item['amount'], 'cbc')
-                ->addAttribute('currencyID', 'SAR');
+            $amount = number_format($item['amount'], 2, '.', '');
+            $vat    = number_format($item['vat_amount'], 2, '.', '');
 
-            $price = $line->addChild('cac:Price', null, 'cac');
-            $price->addChild('cbc:PriceAmount', $item['unit_price'], 'cbc')
-                ->addAttribute('currencyID', 'SAR');
+            $extAmount = $doc->createElement('cbc:LineExtensionAmount', $amount);
+            $extAmount->setAttribute('currencyID', 'SAR');
+            $line->appendChild($extAmount);
 
-            $taxTotal = $line->addChild('cac:TaxTotal', null, 'cac');
-            $tax = $taxTotal->addChild('cac:TaxSubtotal', null, 'cac');
-            $tax->addChild('cbc:TaxAmount', $item['vat_amount'], 'cbc')
-                ->addAttribute('currencyID', 'SAR');
+            $price = self::cac($doc, $line, 'cac:Price');
+            $priceAmount = $doc->createElement('cbc:PriceAmount', $amount);
+            $priceAmount->setAttribute('currencyID', 'SAR');
+            $price->appendChild($priceAmount);
+
+            $tax = self::cac($doc, $line, 'cac:TaxTotal');
+            $taxSub = self::cac($doc, $tax, 'cac:TaxSubtotal');
+            $taxAmount = $doc->createElement('cbc:TaxAmount', $vat);
+            $taxAmount->setAttribute('currencyID', 'SAR');
+            $taxSub->appendChild($taxAmount);
         }
 
         // Totals
-        $total = $xml->addChild('cac:LegalMonetaryTotal', null, 'cac');
-        $total->addChild('cbc:TaxExclusiveAmount', $totals['subtotal'], 'cbc')
-            ->addAttribute('currencyID', 'SAR');
-        $total->addChild('cbc:TaxInclusiveAmount', $totals['total'], 'cbc')
-            ->addAttribute('currencyID', 'SAR');
-        $total->addChild('cbc:PayableAmount', $totals['total'], 'cbc')
-            ->addAttribute('currencyID', 'SAR');
+        $legal = self::cac($doc, $invoice, 'cac:LegalMonetaryTotal');
 
-        return $xml->asXML();
+        $sub = number_format($totals['subtotal'], 2, '.', '');
+        $vat = number_format($totals['vat'], 2, '.', '');
+        $total = number_format($totals['total'], 2, '.', '');
+
+        self::addCbcCurrency($doc, $legal, 'cbc:TaxExclusiveAmount', $sub);
+        self::addCbcCurrency($doc, $legal, 'cbc:TaxInclusiveAmount', $total);
+        self::addCbcCurrency($doc, $legal, 'cbc:PayableAmount', $total);
+
+        return $doc->saveXML();
+    }
+
+    private static function cac($doc, $parent, $name)
+    {
+        $el = $doc->createElement($name);
+        $parent->appendChild($el);
+        return $el;
+    }
+
+    private static function addCbc($doc, $parent, $name, $value)
+    {
+        $el = $doc->createElement($name, $value);
+        $parent->appendChild($el);
+        return $el;
+    }
+
+    private static function addCbcCurrency($doc, $parent, $name, $value)
+    {
+        $el = $doc->createElement($name, $value);
+        $el->setAttribute('currencyID', 'SAR');
+        $parent->appendChild($el);
+        return $el;
     }
 }
